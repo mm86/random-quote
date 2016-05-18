d4f848dfee62c38007a62301ea67e5669016b1ac
Madhu <madhuvanmahesh@gmail.com>
2016-05-17 19:26:55 -0700

    feat: Complete app features
    Two features are built into this app. 'New quote' button generates random quote extracted from forismatic API and 'Twitter' button generates a tweet for the selected quote.

---
+++ app.js
@@ -0,0 +1,55 @@
+
+//on page load, display a quote
+$.ajax({
+      url: "http://api.forismatic.com/api/1.0/",
+      jsonp: "jsonp",
+      dataType: "jsonp",
+      data: {
+        method: "getQuote",
+        lang: "en",
+        format: "jsonp"
+      }
+    })
+    .done(result)
+    .fail(errorHandling);
+
+//on clicking "new quote" button, display a new quote
+$('#new-quote').click(function() {
+  $.ajax({
+      url: "http://api.forismatic.com/api/1.0/",
+      jsonp: "jsonp",
+      dataType: "jsonp",
+      data: {
+        method: "getQuote",
+        lang: "en",
+        format: "jsonp"
+      }
+    })
+    .done(result)
+    .fail(errorHandling);
+});
+
+function result(response) {
+  console.log(response);
+  $('#quote').html(JSON.stringify(response.quoteText));
+  $('#person').html(JSON.stringify(response.quoteAuthor));
+}
+
+function errorHandling(jqxhr, textStatus, err) {
+  console.log("Request Failed: " + textStatus + ", " + err);
+}
+
+//on clicking "Twitter" button, open the twitter link
+$('#twitter').click(function() {
+  var quote = $('#quote').text();
+  var person = $('#person').text();
+  window.open('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + quote + '" ' + person));
+
+});
+
+
+
+
+
+
+
